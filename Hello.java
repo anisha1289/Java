ee35501d38209893e023baab41950bccb791edb6
GOURISANKAR ACHARYA <74904092+Acharya0702@users.noreply.github.com>
2024-03-13 10:56:43 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,6 @@
-import java.util.Scanner;
 public class Hello{
   public static void main(String []args){
     Scanner sc = new Scanner(System.in);
     System.out.println("Hello Java");
-    System.out.println("Enter a num: ");
-    int num = sc.nextInt();
-    System.out.println("The Number is: "+num);
   }
 }
