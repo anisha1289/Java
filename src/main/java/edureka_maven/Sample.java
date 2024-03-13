81559f7d0258640f51486727893e9b2d5d331521
GOURISANKAR ACHARYA <74904092+Acharya0702@users.noreply.github.com>
2024-03-13 21:04:18 +0530

    Add files via upload

---
+++ src/main/java/edureka_maven/Sample.java
@@ -0,0 +1,8 @@
+package edureka_maven;
+
+public class Sample {
+	public String SamMethod() {
+		return "Hello World";
+	}
+
+}
---
+++ src/test/java/edureka_maven/SamMethodtest.java
@@ -0,0 +1,14 @@
+package edureka_maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SamMethodtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
